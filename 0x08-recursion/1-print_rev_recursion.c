03811b3c94f858d9083bcb7ed509fbb461719072
Abdallat988 <abdallat43@icloud.com>
2023-08-03 03:50:08 +0300

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _print_rev_recursion - entry point prints string in reverse
+* @s: pointer
+* Return: void
+*/
+void _print_rev_recursion(char *s)
+{
+	while (*s == '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s--);
+	}
+}
