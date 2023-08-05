d34025457065d0d75dc469c76b17eaf26a30e0ec
Abdallat988 <abdallat43@icloud.com>
2023-08-05 10:00:44 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+* main - entry point
+* @argc: arg counting
+* @argv: argument
+* Return: 0
 */
 int main(int argc, char *argv[])
 {
 	printf("%d %s", argc, argv[0]);
+
+	return (0);
 }
