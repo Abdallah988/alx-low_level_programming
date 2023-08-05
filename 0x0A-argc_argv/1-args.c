1786c73c42b1e40315720b65579513c1ba084ec3
Abdallat988 <abdallat43@icloud.com>
2023-08-05 10:36:26 +0300

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+* @argc: argument counter
+* @argv: argument value
+* Return: 0
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%i\n", argc - 1);
+	//we can remove unused and using void (argv);
+	return (0);
+
+}
